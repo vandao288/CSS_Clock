af98586bf12bbe84b01407cca62f210acb5a9800
Van Dao <daoanhvan81@gmail.com>
2018-03-15 20:59:55 +0700

    add setTime function, clock now work

---
+++ mainClock.js
@@ -0,0 +1,22 @@
+var hourHand = document.querySelector('.hour-hand');
+var minHand = document.querySelector('.min-hand');
+var secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+  var now = new Date();
+
+  var hours = now.getHours();
+  var hourDeg = hours * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+
+  var mins = now.getMinutes();
+  var minDeg = mins * 6 + 90;
+  minHand.style.transform = `rotate(${minDeg}deg)`;
+
+  var seconds = now.getSeconds();
+  var secDeg = seconds * 6 + 90; // secDeg = (seconds / 60) * 360
+  secondHand.style.transform = `rotate(${secDeg}deg)`;
+  console.log(hours + ':' + mins + ':' + seconds);
+}
+
+setInterval(setDate, 1000);
